83795956c637ca55ee4d6f7fca77d4c1ba653bc7
John Alesi Resurreccion <resurreccionjohnalesi@gmail.com>
2021-08-25 21:42:03 +0800

    Added Activitys16

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Output
+//createLoop(1,4)
+/*
+4
+3
+2
+1
+*/
+
+//createLoop(4,3)
+/* Please make sure the 1st number is lower
+than the 2nd number */
+
+/* createloop(5,5)
+The numbers are the same
+*/
+
+function createLoop(num1,num2){
+	
+	if (num1 === num2) {
+		console.log('The numbers are the same');
+	}
+
+	else if (num1 > num2){
+		console.log('Please make sure the 1st number is lower then the 2nd number');	
+	}
+
+
+	for(let num = num2; num >= num1; num--){
+		console.log(num);
+	}
+
+}
