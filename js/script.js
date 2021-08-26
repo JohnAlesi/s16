35d5d01ab188224d1bbc22dbf8bb3a1777395985
John Alesi Resurreccion <resurreccionjohnalesi@gmail.com>
2021-08-26 21:46:29 +0800

    Modified a1

--- js/script.js
+++ js/script.js
@@ -16,18 +16,27 @@ The numbers are the same
 */
 
 function createLoop(num1,num2){
-	
-	if (num1 === num2) {
-		return('The numbers are the same');
-	}
-
-	if (num1 > num2){
-		return('Please make sure the 1st number is lower then the 2nd number');	
-	}
-
-
+	if (num2 > num1) {
 	for(let num = num2; num >= num1; num--){
 		console.log(num);
 	}
+}
+	else if (num1 == num2) {
+		console.log('The numbers are the same');
+	}
+
+	else {
+		console.log('Please make sure the 1st number is lower then the 2nd number');	
+	}
+	
 
 }
+
+
+// function createLoop(a,b){
+	//if(b>a){
+	//for(let i=b;i>=a;i--){console.log(i);}
+	//}
+	//else if(a==b){console.log("The numbers are the same");}
+	//else{console.log("Please make sure that the 1st number is lower than the 2nd");}
+//}
